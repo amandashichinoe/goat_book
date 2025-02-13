cc54c3cc9302718a188a1cf34c505421d6801776
amandashichinoe <amanda.ncosta@hotmail.com>
2025-02-13 07:03:22 -0300

    merged the two tests created earlier, added url config mapping '/' to home_page view

--- lists/tests.py
+++ lists/tests.py
@@ -3,17 +3,8 @@ from django.http import HttpRequest
 from lists.views import home_page
 
 class HomePageTest(TestCase):
-    # Can we make this view function return the HTML we need?
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode("utf8")
-        self.assertIn("<title>To-Do lists</title>", html)
-        self.assertTrue(html.startswith("<html>"))
-        self.assertTrue(html.endswith("</html>"))
-
-
-    def test_home_page_returns_correct_html_2(self):
-        # Can we tell Django to use this view function when make a request for the root of the site ("/")?
         response = self.client.get("/")
         self.assertContains(response, "<title>To-Do lists</title>")
+        self.assertContains(response, "<html>")
+        self.assertContains(response, "</html>")
