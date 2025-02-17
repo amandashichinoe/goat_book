e5494e84d4d4d19fb97fdfd75ca7aa47b7da5621
amandashichinoe <amanda.ncosta@hotmail.com>
2025-02-17 07:27:48 -0300

    saving the POST to the Database, redirect after a POST

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from lists.models import Item
 
 def home_page(request):
+    if request.method == "POST":
+        item = Item()
+        item.text = request.POST["item_text"]
+        item.save()
+        return redirect("/")
+
     return render(request, "home.html", {"new_item_text": request.POST.get("item_text", "")},)
