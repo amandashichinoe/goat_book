4dc2f951ed19b172c2074b0db1330dcc70ec224a
amandashichinoe <amanda.ncosta@hotmail.com>
2025-02-17 08:28:42 -0300

    refactor code smell: Post test is too long

--- lists/views.py
+++ lists/views.py
@@ -3,9 +3,7 @@ from lists.models import Item
 
 def home_page(request):
     if request.method == "POST":
-        item = Item()
-        item.text = request.POST["item_text"]
-        item.save()
+        Item.objects.create(text=request.POST["item_text"])
         return redirect("/")
 
     return render(request, "home.html", {"new_item_text": request.POST.get("item_text", "")},)
